31a8d24cf785fd1b4435317dff6ddf8ed18fea69
GHEUX <theo.gheux@etu.univ-amu.fr>
2025-06-05 13:40:31 +0200

    main

--- SAE201/src/main/java/com/bomberman/Main.java
+++ SAE201/src/main/java/com/bomberman/Main.java
@@ -1,4 +1,24 @@
 package com.bomberman;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/bomberman/fxml/bomb.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/com/bomberman/css/style.css").toExternalForm());
+        primaryStage.setTitle("Bomberman");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
